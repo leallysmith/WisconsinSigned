c5ba4887ece916b50e7ce42ccb19b23aa4682662
leallysmith <89225401+leallysmith@users.noreply.github.com>
2023-10-26 11:22:51 -0600

    no more double google maps

---
+++ layers/2023EnergyCommunities_5.js
@@ -0,0 +1,1 @@
+var json_2023EnergyCommunities_5 = {"type":"FeatureCollection","name":"2023EnergyCommunities_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
--- layers/AdditionalSelectionCriteriaGeographicOption2PersistentPovertyCounty_8.js
+++
@@ -1,1 +0,0 @@
-var json_AdditionalSelectionCriteriaGeographicOption2PersistentPovertyCounty_8 = {"type":"FeatureCollection","name":"AdditionalSelectionCriteriaGeographicOption2PersistentPovertyCounty_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
