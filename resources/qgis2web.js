99671abc4171a7f190d478d1e9b1c1def7571aa6
leallysmith <89225401+leallysmith@users.noreply.github.com>
2023-10-26 09:23:34 -0600

    updating transparency

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -76,7 +76,7 @@ var map = new ol.Map({
 var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
 map.addControl(layerSwitcher);
 
-map.getView().fit([-10488120.973373, 5145292.818537, -9096193.963822, 6052921.701291], map.getSize());
+map.getView().fit([-10656763.449397, 5119926.970715, -9268739.291723, 6039333.910504], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
--- styles/SignedParcelslastupdated102523_15_style.js
+++ styles/SignedParcelslastupdated102523_15_style.js
@@ -9,19 +9,20 @@ var style_SignedParcelslastupdated102523_15 = function(feature, resolution){
     var value = ""
     var labelText = "";
     size = 0;
-    var labelFont = "10px, sans-serif";
-    var labelFill = "#000000";
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
     var bufferColor = "";
     var bufferWidth = 0;
     var textAlign = "left";
     var offsetX = 8;
     var offsetY = 3;
     var placement = 'point';
-    if ("" !== null) {
-        labelText = String("");
+    if (feature.get("Parcel") !== null) {
+        labelText = String(feature.get("Parcel"));
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,1,1,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),
+        image: new ol.style.Circle({radius: 4.0 + size,
+             fill: new ol.style.Fill({color: 'rgba(255,5,1,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
