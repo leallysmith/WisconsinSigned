b676bc98a4ae66be79a4024878b3c7c39c02eb3d
leallysmith <89225401+leallysmith@users.noreply.github.com>
2023-10-30 11:25:32 -0600

    decluttering labels

---
+++ styles/SignedParcelslastupdated102523_14_style.js
@@ -0,0 +1,42 @@
+var size = 0;
+var placement = 'point';
+
+var style_SignedParcelslastupdated102523_14 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    var clusteredFeatures = feature.get("features");
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    size = clusteredFeatures.length;
+    var textAlign = "center";
+    var offsetX = 0;
+    var offsetY = 0;
+    if (size == 1) {
+        textAlign = "left"
+        offsetX = 8
+        offsetY = 3
+        var feature = clusteredFeatures[0];
+        if ("" !== null) {
+            labelText = String("");
+        }
+        key = value + "_" + labelText
+    } else {
+        labelText = size.toString()
+        size = 2*(Math.log(size)/ Math.log(2))
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+             fill: new ol.style.Fill({color: 'rgba(255,5,1,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
